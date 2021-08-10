07828f2ee9b346148f70184f56575b9fc19591b4
Saranthegreat <sar_you@yahoo.com>
2021-08-10 13:57:48 +0800

    Buy'em E-Commerce Platform

---
+++ cart_view.php
@@ -0,0 +1,293 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+	<?php include 'includes/navbar.php'; ?>
+
+	  <div class="content-wrapper">
+	    <div class="container">
+
+	      <!-- Main content -->
+	      <section class="content">
+	        <div class="row">
+	        	<div class="col-sm-9">
+	        		<h1 class="page-header">YOUR CART</h1>
+	        		<div class="box box-solid">
+	        			<div class="box-body">
+		        		<table class="table table-bordered">
+		        			<thead>
+		        				<th></th>
+		        				<th>Photo</th>
+		        				<th>Name</th>
+		        				<th>Price</th>
+		        				<th width="20%">Quantity</th>
+		        				<th>Subtotal</th>
+		        			</thead>
+		        			<tbody id="tbody">
+		        			</tbody>
+		        		</table>
+	        			</div>
+	        		</div>
+					
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.dropbtn {
+  background-color: #3498DB;
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+}
+
+.dropbtn:hover, .dropbtn:focus {
+  background-color: #2980B9;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  overflow: auto;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown a:hover {background-color: #ddd;}
+
+.show {display: block;}
+</style>
+</head>
+<body>
+
+<h2>Select Delivery Location</h2>
+
+<div class="dropdown">
+  <button onclick="myFunction()" class="dropbtn">Delivery Location</button>
+  <div id="myDropdown" class="dropdown-content">
+    <a href="#home">Kuala Lumpur</a>
+    <a href="#about">Singapore</a>
+  </div>
+</div>
+
+<script>
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+</script>
+
+</body>
+</html>
+
+					
+	        		<?php
+	        			if(isset($_SESSION['user'])){
+							echo "<h4> '' <h4> <h4> '' <h4>";
+	        				echo "
+							    
+								<h4> Payment
+								<h4>
+								<div id='paypal-button'></div>
+	        				";
+							
+	        			}
+						
+	        			else{
+	        				echo "
+	        					<h4>You need to <a href='login.php'>Login</a> to checkout.</h4>
+	        				";
+	        			}
+	        		?>
+	        	</div>
+	        	<div class="col-sm-3">
+	        		<?php include 'includes/sidebar.php'; ?>
+	        	</div>
+	        </div>
+	      </section>
+	     
+	    </div>
+	  </div>
+  	<?php $pdo->close(); ?>
+  	<?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+<script>
+var total = 0;
+$(function(){
+	$(document).on('click', '.cart_delete', function(e){
+		e.preventDefault();
+		var id = $(this).data('id');
+		$.ajax({
+			type: 'POST',
+			url: 'cart_delete.php',
+			data: {id:id},
+			dataType: 'json',
+			success: function(response){
+				if(!response.error){
+					getDetails();
+					getCart();
+					getTotal();
+				}
+			}
+		});
+	});
+
+	$(document).on('click', '.minus', function(e){
+		e.preventDefault();
+		var id = $(this).data('id');
+		var qty = $('#qty_'+id).val();
+		if(qty>1){
+			qty--;
+		}
+		$('#qty_'+id).val(qty);
+		$.ajax({
+			type: 'POST',
+			url: 'cart_update.php',
+			data: {
+				id: id,
+				qty: qty,
+			},
+			dataType: 'json',
+			success: function(response){
+				if(!response.error){
+					getDetails();
+					getCart();
+					getTotal();
+				}
+			}
+		});
+	});
+
+	$(document).on('click', '.add', function(e){
+		e.preventDefault();
+		var id = $(this).data('id');
+		var qty = $('#qty_'+id).val();
+		qty++;
+		$('#qty_'+id).val(qty);
+		$.ajax({
+			type: 'POST',
+			url: 'cart_update.php',
+			data: {
+				id: id,
+				qty: qty,
+			},
+			dataType: 'json',
+			success: function(response){
+				if(!response.error){
+					getDetails();
+					getCart();
+					getTotal();
+				}
+			}
+		});
+	});
+
+	getDetails();
+	getTotal();
+
+});
+
+function getDetails(){
+	$.ajax({
+		type: 'POST',
+		url: 'cart_details.php',
+		dataType: 'json',
+		success: function(response){
+			$('#tbody').html(response);
+			getCart();
+		}
+	});
+}
+
+function getTotal(){
+	$.ajax({
+		type: 'POST',
+		url: 'cart_total.php',
+		dataType: 'json',
+		success:function(response){
+			total = response;
+		}
+	});
+}
+</script>
+<!-- Paypal Express -->
+<script>
+paypal.Button.render({
+    env: 'sandbox', // change for production if app is live,
+
+	client: {
+        sandbox:    'ASb1ZbVxG5ZFzCWLdYLi_d1-k5rmSjvBZhxP2etCxBKXaJHxPba13JJD_D3dTNriRbAv3Kp_72cgDvaZ',
+        //production: 'AaBHKJFEej4V6yaArjzSx9cuf-UYesQYKqynQVCdBlKuZKawDDzFyuQdidPOBSGEhWaNQnnvfzuFB9SM'
+    },
+    
+//	onAuthorize: function(data, actions) {
+//        return actions.payment.execute().then(function(payment) {
+//			window.location = 'sales.php?pay='+payment.id;
+//        });
+		
+    commit: true, // Show a 'Pay Now' button
+
+    style: {
+    	color: 'gold',
+    	size: 'small'
+    },
+
+    payment: function(data, actions) {
+        return actions.payment.create({
+            payment: {
+                transactions: [
+                    {
+                    	//total purchase
+                        amount: { 
+                        	total: total, 
+                        	currency: 'USD' 
+                        }
+                    }
+                ]
+            }
+        });
+    },
+
+    onAuthorize: function(data, actions) {
+        return actions.payment.execute().then(function(payment) {
+			window.location = 'sales.php?pay='+payment.id;
+        });
+    },
+
+}, '#paypal-button');
+</script>
+</body>
+
+</html>
